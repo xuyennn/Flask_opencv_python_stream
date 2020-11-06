0594204908f7b77b8576c0a51dca0206835b0cb7
xuyennn <54520556+xuyennn@users.noreply.github.com>
2020-11-06 14:41:50 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import cv2
+from flask import Flask , Response , render_template
+
+#cam = cv2.VideoCapture("tracking.mp4")
+cam = cv2.VideoCapture(0)
+app = Flask(__name__)
+
+def stream():
+    while 1 :
+        __,frame = cam.read()
+        imgencode = cv2.imencode('.jpeg',frame)[1]
+        strinData = imgencode.tostring()
+        yield (b'--frame\r\n'b'Content-Type: text/plain\r\n\r\n'+strinData+b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(stream(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
